40186d1b73252503d91572bf44a366afce44ffeb
Yubo-Cao <cao2006721@gmail.com>
2023-06-23 11:54:47 -0400

    chore: add __pycache__

--- game/player.py
+++ game/player.py
@@ -17,44 +17,55 @@ class Player(pg.sprite.Sprite, PlayInstance):
     score = ObservableDescriptor[int]()
     health = ObservableDescriptor[int]()
 
+    # player state
+    class State(Enum):
+        WALK = 0
+        JUMP = 1
+        DIVE = 2
+
     def __init__(
-        self,
-        keymap=None,
-        speed: float = 16,
-        gravity: float = 1,
+            self,
+            speed: float = 16,
+            gravity: float = 1,
     ) -> None:
         super().__init__()
-        if keymap is None:
-            keymap = {
-                pg.K_LEFT: Direction.LEFT,
-                pg.K_RIGHT: Direction.RIGHT,
-                pg.K_UP: Direction.TOP,
-                pg.K_DOWN: Direction.BOTTOM,
-            }
         self.image = load_im("player.png", scale=48 / 2048)
         self.rect: pg.Rect = self.image.get_rect()
-        self.keymap = keymap
         self.speed = speed
         self.gravity = gravity
         self.x: float = 0
         self.y: float = 0
         self.velocity: tuple[float, float] = [0, 0]
+        self.state: Player.State = Player.State.WALK
 
     def update(self, dt: float):
         self.velocity = [0, 0]
         keys = pg.key.get_pressed()
-        for key, direction in self.keymap.items():
-            if keys[key]:
-                self.move(direction)
-        self.velocity = [self.velocity[0], self.velocity[1] + self.gravity]
-        self.apply_velocity(dt)
+        match self.state:
+            case Player.State.WALK:
+                self.__handle_move(keys)
+                if keys[pg.K_SPACE]:
+                    self.state = Player.State.JUMP
+                    self.velocity = (self.velocity[0], self.velocity[1] - 32)
+            case Player.State.JUMP:
+                self.__handle_move(keys)
 
-    def apply_velocity(self, dt: float):
+        # apply gravity
+        self.velocity = [self.velocity[0], self.velocity[1] + self.gravity]
+        self.__apply_velocity(dt)
+
+    def __handle_move(self, keys):
+        if keys[pg.K_LEFT]:
+            self.__move(Direction.LEFT)
+        if keys[pg.K_RIGHT]:
+            self.__move(Direction.RIGHT)
+
+    def __apply_velocity(self, dt: float):
         self.x += self.velocity[0] * dt
         self.y += self.velocity[1] * dt
         self.__sync_rect()
 
-    def move(self, direction: Direction):
+    def __move(self, direction: Direction):
         if direction == Direction.LEFT:
             self.velocity = (self.velocity[0] - self.speed, self.velocity[1])
         elif direction == Direction.RIGHT:
@@ -65,6 +76,7 @@ class Player(pg.sprite.Sprite, PlayInstance):
             self.velocity = (self.velocity[0], self.velocity[1] + self.speed)
 
     def render(self, surface: pg.Surface):
+        # TODO: have different sprite for each state
         surface.blit(self.image, self.rect)
 
     def wall_collide(self, collison: Collision):
